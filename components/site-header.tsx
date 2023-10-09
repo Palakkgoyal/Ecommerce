0396a1a01bab6b49ad77b8651adf288b8c498ab9
Palak <Palakgoyal0304@gmail.com>
2023-10-09 19:08:33 +0530

    Add search bar and cart count

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,18 +12,31 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const { cartCount } = useShoppingCart()
+  const defaultSearchQuery = searchParams.get('search') ?? ""
+
   if(pathname.startsWith('/studio')) return null
+
+  function onSubmit(event: React.SyntheticEvent<HTMLFormElement>){
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const searchQuery = formData.get('search')
+    router.replace(`/?search=${searchQuery}`)
+  }
   
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
+        <form onSubmit={onSubmit} className="hidden items-center lg:inline-flex">
           <Input
             id="search"
             name="search"
             type="search"
             autoComplete="off"
+            defaultValue={defaultSearchQuery}
             placeholder="Search products..."
             className="h-9 lg:w-[300px]"
           />
@@ -32,7 +45,7 @@ export function SiteHeader() {
           <Link href="/cart">
             <Button size="sm" variant="ghost">
               <ShoppingBag className="h-5 w-5" />
-              <span className="ml-2 text-sm font-bold">0</span>
+              <span className="ml-2 text-sm font-bold">{cartCount}</span>
               <span className="sr-only">Cart</span>
             </Button>
           </Link>
