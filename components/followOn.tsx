55ef839c8fe3222438eb9c8875ecd0783991ef39
Palak <Palakgoyal0304@gmail.com>
2023-10-13 07:33:13 +0000

    fremove sociable kit widget

--- components/followOn.tsx
+++ components/followOn.tsx
@@ -7,7 +7,7 @@ export default function FollowOn() {
         <div className="flex flex-col gap-[20px] px-4 mt-[80px]">
             <h2 className="text-2xl font-bold tracking-normal">Follow On: </h2>
             <div className="flex flex-col gap-[10px] lg:flex-row">
-                <iframe src='https://widgets.sociablekit.com/linkedin-profile-posts/iframe/204773' frameBorder='0' scrolling='no' className="border-[2px] w-full h-[380px] md:w-[500px] md:h-[250px]"></iframe>
+                {/* <iframe src='https://widgets.sociablekit.com/linkedin-profile-posts/iframe/204773' frameBorder='0' scrolling='no' className="border-[2px] w-full h-[380px] md:w-[500px] md:h-[250px]"></iframe> */}
                 <iframe src='https://widgets.sociablekit.com/instagram-feed/iframe/204793' frameBorder='0' scrolling='no' className="border-[2px] w-full h-[380px] md:w-[500px] md:h-[250px]"></iframe>
             </div>
             <Link href="/about">
