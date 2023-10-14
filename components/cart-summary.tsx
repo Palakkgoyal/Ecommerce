3a0c9c05a830244389036e55dcea11c4bce438af
Palak <Palakgoyal0304@gmail.com>
2023-10-14 07:38:31 +0000

    fix curreny in cart summary

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -7,7 +7,7 @@ import { formatCurrencyString, useShoppingCart } from "use-shopping-cart"
 import { Button } from "@/components/ui/button"
 
 export function CartSummary() {
-  const { formattedTotalPrice, totalPrice, cartDetails, cartCount, redirectToCheckout } = useShoppingCart()
+  const { totalPrice, cartDetails, cartCount, redirectToCheckout } = useShoppingCart()
   const [isLoading, setLoading] = useState(false)
   const isDisabled = isLoading || cartCount! === 0
   const shippingAmount = cartCount! > 0 ? 500 : 0
@@ -40,7 +40,7 @@ export function CartSummary() {
       <dl className="mt-6 space-y-4">
         <div className="flex items-center justify-between">
           <dt className="text-sm">Subtotal</dt>
-          <dd className="text-sm font-medium">{formattedTotalPrice}</dd>
+          <dd className="text-sm font-medium">{formatCurrencyString({ value: totalPrice!, currency: 'INR'})}</dd>
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
           <dt className="flex items-center text-sm">
@@ -50,7 +50,7 @@ export function CartSummary() {
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
           <dt className="text-base font-medium">Order total</dt>
-          <dd className="text-base font-medium">{formatCurrencyString({ value: totalAmount, currency: 'USD'})}</dd>
+          <dd className="text-base font-medium">{formatCurrencyString({ value: totalAmount, currency: 'INR'})}</dd>
         </div>
       </dl>
 
