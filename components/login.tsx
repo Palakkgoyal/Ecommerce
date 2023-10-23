fc8b062b8bdeb2c2fdc8204b4fdd8e1bc50d26ad
Palak <Palakgoyal0304@gmail.com>
2023-10-23 07:03:57 +0000

    fix login menu

--- components/login.tsx
+++ components/login.tsx
@@ -4,13 +4,13 @@ import { Button } from "@/components/ui/button"
 
 export default function Login() {
     return (
-        <div className="flex w-full items-center justify-center">
-            <div className="mt-10 border-[2px]">
-                <h1>Login/Signup</h1>
+        <div className="flex w-full mt-10 items-center justify-center">
+            <div className="mt-10 border-[2px] p-5 rounded-md shadow-md">
+                <h1 className="text-center text-xl font-medium mb-3">Login/Signup</h1>
                 <p>Please login to continue</p>
                 <Link href="/api/auth/login">
                     <Button type="button"
-                        className="bg-violet-600 py-6 text-base font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500"
+                        className="bg-violet-600 mt-2 w-full py-6 text-base font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500"
                     >
                         Login
                     </Button>
