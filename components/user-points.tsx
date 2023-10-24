b740d7754cb5669beca936189f8cbbd617141379
Palak <Palakgoyal0304@gmail.com>
2023-10-24 08:05:51 +0000

    add user points ui on n user page

--- components/user-points.tsx
+++ components/user-points.tsx
@@ -1,10 +1,19 @@
 import React from "react"
-import { Star } from "lucide-react"
+import starImg from "@/public/products/star.png"
+import Image from "next/image"
 
 export default function UserPoints() {
     return (
-        <div>
-            <Star />
+        <div className="p-5 max-w-7xl flex justify-end items-center">
+            <div className="border-[2px] shadow-md flex px-2 py-[6px] rounded-lg bg-slate-100 gap-[4px]">
+                <Image
+                    src={starImg}
+                    width={25}
+                    height={25}
+                    alt="points"
+                />
+                <p className="font-semibold">20</p>
+            </div>
         </div>
     )
 }
