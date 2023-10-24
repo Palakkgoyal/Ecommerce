09a4d64d543ce5ae970854a917fc8f803b1df1ca
Palak <Palakgoyal0304@gmail.com>
2023-10-24 07:21:47 +0000

    display user orders on user page

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import Home from "@/components/pay-now"
 import { getSession } from '@auth0/nextjs-auth0';
-// import { useRouter } from "next/router";
 
 export default async function Page() {
     const res = await getSession();
     const user = res?.user
-    // const router = useRouter()
 
     if(!user) return null
     
--- components/user-orders.tsx
+++ components/user-orders.tsx
@@ -5,13 +5,17 @@ import { groq } from "next-sanity"
 import { SanityProduct } from "@/config/inventory"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function UserOrders() {
-    const email = 'palakgoyal0304@gmail.cm'
-    const [orders, setOrders] = useState<SanityProduct[]>([])
+    const [allOrders, setAllOrders] = useState<SanityProduct[]>([])
+    const { user, error, isLoading } = useUser();
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
 
     async function getOrders() {
-        const orders = await client.fetch<SanityProduct[]>(groq`*[_type=="order" && email == '${email}'] {
+        const orders = await client.fetch<SanityProduct[]>(groq`*[_type=="order" && email == '${user?.email}'] {
             _id,
             _createdAt,
             name,
@@ -21,14 +25,15 @@ export default function UserOrders() {
             total_amt,
             coupon_code,
           }`)
-        setOrders(orders)
+        setAllOrders(orders)
     }
 
     useEffect(() => {
         getOrders()
     }, [])
 
-    console.log(orders)
+    console.log(allOrders)
+
 
     return (
         <div className="my-10 flex items-center justify-center border-[2px] p-5">
@@ -37,14 +42,40 @@ export default function UserOrders() {
                     <h2 className="text-lg font-bold">Your Orders</h2>
                 </div>
                 <div>
-                    {orders.length === 0 ? (
+                    {allOrders.length === 0 ? (
                         <div className="mt-8">
                             <Link href="/products">
                                 <Button variant="default">Let{`'`}s do some shopping 🛒</Button>
                             </Link>
                         </div>
                     ) : (
-                        <div></div>
+                        <div className="mt-8 flex gap-4 flex-wrap text-sm">
+                            {allOrders.map((order, idx) => {
+                                const { _id, orders, total_amt, address } = order
+                                return (
+                                    <div key={_id} className="border-[2px] flex flex-col gap-2 w-full max-w-[400px] shadow-sm p-5 rounded-lg">
+                                        <dl className="space-y-4">
+                                            <div className="flex items-center justify-between">
+                                                <dt className="text-base">Total Amount: </dt>
+                                                <dd className="text-base font-medium">{total_amt}₹</dd>
+                                            </div>
+                                        </dl>
+                                        <p className="text-sm"><span className="font-semibold">Address: </span>{address[0].line1}, {address[0].city}...</p>
+                                        <div>
+                                            <h3 className="text-center font-semibold mb-2">Items</h3>
+                                            <div className="flex flex-wrap gap-3">
+                                                {orders.map((item: any) => (
+                                                    <div key={item._key} className="shadow-md p-2 border-[1px] rounded-md">
+                                                        <p>Quantity: {item.quantity}</p>
+                                                        <Link href={`/products/${item.slug}`} className="text-blue-500 underline">{item.slug}</Link>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     )}
                 </div>
             </div>
